4f0ded9464e1ca07d36c0a8346ba46c7d153ddb3
kumaramit <kumaramit710@gmail.com>
2021-12-04 07:08:52 +0530

    remove middleware stop on 10 counter

--- src/store.js
+++ src/store.js
@@ -3,17 +3,18 @@ import thunk from "redux-thunk";
 
 import rootReducers from "./reducers";
 
-const firstmiddleware = (state) => (next) => (action) => {
+/*const firstmiddleware = (state) => (next) => (action) => {
     //console.log(store.getState().countReducers)
   if (store.getState().countReducers >= 10) {
     return next({ type: "DECREMENT" });
   }
   next(action);
-};
+};*/
 
 const store = createStore(
   rootReducers,
-  applyMiddleware(firstmiddleware, thunk)
+  applyMiddleware( thunk )
+  //applyMiddleware(firstmiddleware, thunk)
 );
 
 export default store;
